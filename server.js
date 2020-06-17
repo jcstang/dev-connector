7e7a0ba93fedf092ad917d7bd51c49c46c231317
Jacob Stanger <jstangwork@gmail.com>
2020-06-17 11:36:07 -0600

    init commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API running'));
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
