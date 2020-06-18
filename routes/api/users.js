f863f158a188e5001115f08d1282c7aba4e93a55
Jacob Stanger <jstangwork@gmail.com>
2020-06-18 17:17:33 -0600

    .

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', [
+  check('name', 'name is required').not().isEmpty(),
+  check('email', 'please include valid email').isEmail(),
+  check('password', 'please enter a password with 6 or more characters').isLength({ min: 6 })
+], (req, res) => {
+  // express-validator uses an array of errors.
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  res.status(200).send('successfully registered user........');
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // connect database
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'));
 
 // Define Routes
