81f20382ae6b28b5146b2219347c3f62430c1370
Jacob Stanger <jacobcstanger@gmail.com>
2020-06-30 13:44:55 -0600

    jwt getting user back wo password

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+// middleware function
+module.exports = function(req, res, next) {
+    // 1. get token from header
+    // header key we want to send the token in
+    const token = req.header('x-auth-token');
+
+    // 2. check if not token
+    if(!token) {
+      return res.status(401).json({ msg: 'no token, authorization denied.'});
+    }
+
+    // 3. verify token
+    try {
+      const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+      req.user = decoded.user;
+      next();
+    } catch (err) {
+      res.status(401).json({ msg: 'Token is not valid' });
+    }
+
+}
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => res.send('auth route'));
+// adding the 'auth' middleware makes this a protected route
+// router.get('/', auth, (req, res) => res.send('auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    // this line gives an error of .select not a function
+    // const user = await (await User.findById(req.user.id)).select('-password');
+    const user = await (await User.findById(req.user.id, '-password'));
+    res.json(user);
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
+
+});
 
 module.exports = router;
