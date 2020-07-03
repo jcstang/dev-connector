06a69333708c3a5add1df3609ea5f6c12af6aa3c
Jacob Stanger <jacobcstanger@gmail.com>
2020-07-02 20:53:25 -0600

    add post route for api/profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
 
-const Profile = require('../../models/');
+const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
 // @route   GET api/profile/me
@@ -27,6 +28,63 @@ router.get('/me', auth, async (req, res) => {
     console.error(error.message);
     res.status(500).send('server error');
   }
+});
+
+// @route   POST api/profile
+// @desc    Create or update user profile.
+// @access  Private
+router.post('/', [auth, [
+  check('status', 'Status is required').not().isEmpty(),
+  check('skills', 'Skills is required').not().isEmpty()
+]], 
+async (req, res) => {
+  const errors = validationResult(req);
+  // if there are errors
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {
+    company,
+    website,
+    location,
+    bio,
+    status,
+    githubusername,
+    skills,
+    youtube,
+    facebook,
+    twitter,
+    instagram,
+    linkedin
+  } = req.body;
+
+  // Build profile object
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if (company) profileFields.company = company;
+  if (website) profileFields.website = website;
+  if (location) profileFields.location = location;
+  if (bio) profileFields.bio = bio;
+  if (status) profileFields.status = status;
+  if (githubusername) profileFields.githubusername = githubusername;
+  if (skills) {
+    // convert comma seperated string to an array
+    // also map each value and trim it of spaces 
+    profileFields.skills = skills.split(',').map(skill => skill.trim() );
+  }
+
+  // Build social object
+  profileFields.social = {}
+  if (youtube) profileFields.social.youtube = youtube;
+  if (twitter) profileFields.social.twitter = twitter;
+  if (facebook) profileFields.social.facebook = facebook;
+  if (linkedin) profileFields.social.linkedin = linkedin;
+  if (instagram) profileFields.social.instagram = instagram;
+
+  res.send('Hello');
+
+
 });
 
 module.exports = router;
