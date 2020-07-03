37360bfb2454522d8686883a200b42e1e88565a4
Jacob Stanger <jacobcstanger@gmail.com>
2020-07-03 11:57:33 -0600

    profile ready for postman test

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,91 +1,89 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const auth = require('../../middleware/auth');
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const { check, validationResult } = require('express-validator');
+const bcrypt = require("bcryptjs");
+const auth = require("../../middleware/auth");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
 
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
 // adding the 'auth' middleware makes this a protected route
 // router.get('/', auth, (req, res) => res.send('auth route'));
-router.get('/', auth, async (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
     // this line gives an error of .select not a function
     // const user = await (await User.findById(req.user.id)).select('-password');
-    const user = await (await User.findById(req.user.id, '-password'));
+    const user = await await User.findById(req.user.id, "-password");
     res.json(user);
-  } catch(err) {
+  } catch (err) {
     console.error(err.message);
-    res.status(500).send('server error');
+    res.status(500).send("server error");
   }
-
 });
-
 
 // @route   POST api/auth
 // @desc    Authenticate user & get token
 // @access  Public
-router.post('/', [
-  check('email', 'Please include valid email').isEmail(),
-  check('password', 'Password is required').exists()
-], async (req, res) => {
-
-  // ** check for errors on user input
-  // express-validator uses an array of errors.
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  // ** destructring req.body - for ease of use
-  const { email, password } = req.body;
-
-
-
-  try {
-    let user = await User.findOne({ email });
-
-    if (!user) {
-      res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+router.post(
+  "/",
+  [
+    check("email", "Please include valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    // ** check for errors on user input
+    // express-validator uses an array of errors.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    // user found check if password matches
-    const isMatch = await bcrypt.compare(password, user.password);
+    // ** destructring req.body - for ease of use
+    const { email, password } = req.body;
 
-    if(!isMatch) {
-      res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
-    }
+    try {
+      let user = await User.findOne({ email });
 
-    // return jsonwebtoken
-    // payload to pass to sign func
-    const payload = {
-      user: {
-        id: user.id
+      if (!user) {
+        res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
       }
+
+      // user found check if password matches
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // return jsonwebtoken
+      // payload to pass to sign func
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360001 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+      // res.send('user registered');
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("server error");
     }
 
-    jwt.sign(
-      payload, 
-      config.get('jwtSecret'),
-      { expiresIn: 360001 },
-      (err, token) => {
-        if(err) throw err;
-        res.json({ token });
-      }
-    );
-    // res.send('user registered');
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).send('server error');
+    // res.status(200).send('successfully registered user........');
   }
-
-
-  // res.status(200).send('successfully registered user........');
-});
+);
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,90 +1,124 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../../middleware/auth');
-const { check, validationResult } = require('express-validator/check');
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator/check");
 
-const Profile = require('../../models/Profile');
-const User = require('../../models/User');
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
 // @access  Private
-router.get('/me', auth, async (req, res) => {
+router.get("/me", auth, async (req, res) => {
   try {
-    const profile = await Profile.findOne(
-      { user: req.user.id }
-      ).populate(
-        'user', 
-        ['name', 'avatar']
-      );
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
 
-    if(!profile) {
-      return res.status(400).json({ msg: 'There is no profile for this user' });
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
     }
 
     res.json(profile);
-
   } catch (error) {
     console.error(error.message);
-    res.status(500).send('server error');
+    res.status(500).send("server error");
   }
 });
 
 // @route   POST api/profile
 // @desc    Create or update user profile.
 // @access  Private
-router.post('/', [auth, [
-  check('status', 'Status is required').not().isEmpty(),
-  check('skills', 'Skills is required').not().isEmpty()
-]], 
-async (req, res) => {
-  const errors = validationResult(req);
-  // if there are errors
-  if(!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    // 1. Check for body errors
+    // =============================================================
+    const errors = validationResult(req);
+    // if there are errors
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // 2. Pull out variables from the body
+    // =============================================================
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    // 3a. Build profile object
+    // =============================================================
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      // convert comma seperated string to an array
+      // also map each value and trim it of spaces
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+    }
+
+    // 3b. Build social object
+    // =============================================================
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    // 4. (a)UPDATE existing profile OR (b)CREATE a new one
+    // =============================================================
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile) {
+        // 4a. UPDATE
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+
+      // 4b. CREATE
+      profile = new Profile(profileFields);
+
+      // 5. Save to db
+      // =============================================================
+      await Profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+
+    res.send("Hello");
   }
-
-  const {
-    company,
-    website,
-    location,
-    bio,
-    status,
-    githubusername,
-    skills,
-    youtube,
-    facebook,
-    twitter,
-    instagram,
-    linkedin
-  } = req.body;
-
-  // Build profile object
-  const profileFields = {};
-  profileFields.user = req.user.id;
-  if (company) profileFields.company = company;
-  if (website) profileFields.website = website;
-  if (location) profileFields.location = location;
-  if (bio) profileFields.bio = bio;
-  if (status) profileFields.status = status;
-  if (githubusername) profileFields.githubusername = githubusername;
-  if (skills) {
-    // convert comma seperated string to an array
-    // also map each value and trim it of spaces 
-    profileFields.skills = skills.split(',').map(skill => skill.trim() );
-  }
-
-  // Build social object
-  profileFields.social = {}
-  if (youtube) profileFields.social.youtube = youtube;
-  if (twitter) profileFields.social.twitter = twitter;
-  if (facebook) profileFields.social.facebook = facebook;
-  if (linkedin) profileFields.social.linkedin = linkedin;
-  if (instagram) profileFields.social.instagram = instagram;
-
-  res.send('Hello');
-
-
-});
+);
 
 module.exports = router;
