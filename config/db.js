0bc76d758ee0d62b2b1ed49a5b5da47de9ab713c
Jacob Stanger <jacobcstanger@gmail.com>
2020-07-03 18:37:36 -0600

    updated mongoose settings

--- config/db.js
+++ config/db.js
@@ -1,21 +1,22 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
+      // useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
     });
 
-    console.log('mongoDB connected....');
+    console.log("mongoDB connected....");
   } catch (err) {
     console.log(err.message);
     // Exit process with failure
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
